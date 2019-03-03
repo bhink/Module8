bf7321b30a81b1e1aa37ac6c3bdf0cea16bbde57
bhink <31259065+bhink@users.noreply.github.com>
2019-03-03 15:17:29 -0500

    Add files via upload

---
+++ End_of_year_countdown.py
@@ -0,0 +1,7 @@
+import datetime
+from datetime import datetime, timedelta
+start = datetime.now() #this will start from the current date and time
+end = datetime(int(2019),int(4), int(28), int(11)) #this is the end of spring semester 2019
+End_of_semester = end-start
+print(str(End_of_semester),'Until school is out for summer!')
+if start > end : print(str(End_of_semester), 'School is out for summer!!!!!') #this will print after the semester schedule is over
