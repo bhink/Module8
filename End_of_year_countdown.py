0ffad04136315a8be3a036fbf13cc3715fddcd74
bhink <31259065+bhink@users.noreply.github.com>
2019-04-06 09:57:54 -0400

    adding tests

--- End_of_year_countdown.py
+++ End_of_year_countdown.py
@@ -5,3 +5,13 @@ end = datetime(int(2019),int(4), int(28), int(11)) #this is the end of spring se
 End_of_semester = end-start
 print(str(End_of_semester),'Until school is out for summer!')
 if start > end : print(str(End_of_semester), 'School is out for summer!!!!!') #this will print after the semester schedule is over
+
+ #assert proper values
+pip instsll pytest
+import pytest with pytest.raises(ValueError):
+    convert_to_datetime(1)
+    
+with pytest.raises(ValueError):
+    convert_to_datetime([1, 2])    
+
+assert convert_to_datetime("2019-03-31") == datetime(2019, 3, 31)
